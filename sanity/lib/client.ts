ae257125d02ca17f3399eede913785734e4d3121
radoslavsapundzhiev <radoslavsapundzhiev@gmail.com>
2023-10-31 14:18:14 +0200

    Seed products data

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -7,4 +7,5 @@ export const client = createClient({
   dataset,
   projectId,
   useCdn,
+  token: "sktRGl6GBsFPPovAdIRfSrMwNTn8YncsAgsAIVxZ8eKfkURmi5gUAybogRNzDy2shvSKcRQ9NGvNly8RczRHYYQ2lvOwcZ5AtI3p2iiv5jvmgwCJFMqzmtSWPnM0DRASOnD9Te5spONsnc8Gj7EVDAJdvlaMPOMT4ilAX9lBHvdQd1Lq6wt0"
 })
