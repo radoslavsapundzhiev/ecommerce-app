d7c637e3b3baa461d3342ef96b6270010cc41d6c
radoslavsapundzhiev <radoslavsapundzhiev@gmail.com>
2023-11-01 15:04:58 +0200

    Added product filtering in query in groq query

--- app/page.tsx
+++ app/page.tsx
@@ -12,17 +12,28 @@ import { seedSanityData } from "@/lib/seed"
 interface Props {
   searchParams: {
     date?: string,
-    price?: string
+    price?: string,
+    color?: string,
+    category?: string,
+    size?: string
   }
 }
 
 export default async function Page({ searchParams }: Props) {
-  const {date = "desc", price} = searchParams;
+  const {date = "desc", price, color, category, size} = searchParams;
   const priceOrder = price ? `| order(price ${price})` : "";
   const dateOrder = date ? `| order(_createdAt ${date})` : "";
   const order = `${priceOrder}${dateOrder}`;
+
+  const productFilter = `_type == "product"`;
+  const colorFilter = color ? `&& "${color}" in colors` : "";
+  const categoryFilter = category ? `&& "${category}" in categories` : "";
+  const sizeFilter = size ? `&& "${size}" in sizes` : "";
+
+  const filter = `*[${productFilter}${colorFilter}${categoryFilter}${sizeFilter}]`;
+
   const products = await client.fetch<SanityProduct[]>(
-    groq`*[_type == "product"] ${order} {
+    groq`${filter} ${order} {
       _id,
       _createdAt,
       name,
