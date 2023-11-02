e6af61af670dc43ca2718c705a6807274f45bf0e
radoslavsapundzhiev <radoslavsapundzhiev@gmail.com>
2023-11-02 20:43:41 +0200

    Implemented addToCart() function to add product item to cart, display count of items and display toast

--- components/product-info.tsx
+++ components/product-info.tsx
@@ -16,9 +16,31 @@ interface Props {
 
 export function ProductInfo({ product }: Props) {
   const [selectedSize, setSelectedSize] = useState(product.sizes[0]);
+  const { addItem, incrementItem, cartDetails } = useShoppingCart();
+  const { toast } = useToast();
+  const isInCart = !!cartDetails?.[product._id];
 
-
-  function addToCart() {}
+  function addToCart() {
+    const item = {
+      ...product,
+      product_data: {
+        size: selectedSize
+      }
+    }
+    isInCart ? incrementItem(item._id) : addItem(item);
+    toast({
+      title: `${item.name} (${getSizeName(selectedSize)})`,
+      description: "Product added to cart",
+      action: (
+        <Link href="/cart">
+          <Button variant="link" className="gap-x-2 whitespace-nowrap">
+            <span>Open cart</span>
+            <ArrowRight className="h-5 w-5"/>
+          </Button>
+        </Link>
+      )
+    })
+  }
 
   return (
     <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
@@ -48,6 +70,7 @@ export function ProductInfo({ product }: Props) {
       <form className="mt-6">
         <div className="mt-4 flex">
           <Button
+            onClick={addToCart}
             type="button"
             className="w-full bg-violet-600 py-6 text-base font-medium text-white hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-violet-500"
           >
